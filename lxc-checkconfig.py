e38d577a8a1453197232c439408cd225c3071621
Miha Zidar <zidarsk8@gmail.com>
2014-09-04 00:04:22 +0200

    Simplified get_cgroup_mount_path
    the awk command returns all uncomented lines taht start with the search
    patern, and the ' | head -n 1 ' just takes the first argument. So this
    is a sipmlified version of the function for better readability.
    
    Also removed an unused import from my prev commit

--- lxc-checkconfig.py
+++ lxc-checkconfig.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import os.path
 import platform
-from collections import defaultdict
 import re
 import json
 import gzip
-import sys
+from collections import defaultdict
 
 config = '/proc/config.gz'
 
@@ -36,10 +35,9 @@ def is_enabled(config_name, mandatory=None):
 def get_cgroup_mount_path(search_for,search_where):
     allmounts = open(search_where,'r')
     for mount_line in allmounts:
-        if not mount_line.startswith('#'):
-            mls = mount_line.split(' ')
-            if mls[2] == search_for:
-                return mls[1]
+        if mount_line.strip().startswith(search_for):
+            return mount_line.split(' ')[1]
+    return ''
 
 def print_config(config_dict):
     print_groups = {
